5ba6ccfdc19190a5cd21c23b216a4dfcf0700b2f
Lunar <287263840+locallunarsv@users.noreply.github.com>
2026-08-02 04:11:46 +0800

    refactor: rename identity aggregate module

--- crates/localid-identity/src/identity/mod.rs
+++ crates/localid-identity/src/identity/mod.rs
@@ -1,9 +1,9 @@
-mod entity;
+mod aggregate;
 mod error;
 mod id;
 mod lifecycle_state;
 
-pub use entity::Identity;
+pub use aggregate::Identity;
 pub use error::IdentityError;
 pub use id::IdentityId;
 pub use lifecycle_state::LifecycleState;
