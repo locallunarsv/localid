44cf4d90bd0ff1b4a880b7a628ddf69be6767706
Lunar <287263840+locallunarsv@users.noreply.github.com>
2026-08-20 03:04:01 +0800

    remove obsolete memory repository delegates

--- crates/localid-api/src/bootstrap/repository/client_authentication.rs
+++ crates/localid-api/src/bootstrap/repository/client_authentication.rs
@@ -4,17 +4,7 @@ use localid_database_postgres::{DatabaseError, PostgresOAuthClientRepository};
 
 use localid_oauth_client::{OAuthClient, OAuthClientRepository};
 
-use localid_oauth_client_repository_memory::MemoryOAuthClientRepository;
-
 use super::SharedRepository;
-
-impl ClientAuthenticationPort for SharedRepository<MemoryOAuthClientRepository> {
-    type Error = ();
-
-    fn find_client(&self, client_id: &str) -> Result<Option<OAuthClient>, Self::Error> {
-        self.with(|repository| repository.find_by_client_id(client_id))
-    }
-}
 
 impl ClientAuthenticationPort for SharedRepository<PostgresOAuthClientRepository> {
     type Error = DatabaseError;
--- crates/localid-api/src/bootstrap/repository/oauth_client.rs
+++ crates/localid-api/src/bootstrap/repository/oauth_client.rs
@@ -1,29 +1,7 @@
+use localid_database_postgres::{DatabaseError, PostgresOAuthClientRepository};
 use localid_oauth_client::{OAuthClient, OAuthClientId, OAuthClientRepository};
 
 use super::SharedRepository;
-
-use localid_database_postgres::{DatabaseError, PostgresOAuthClientRepository};
-use localid_oauth_client_repository_memory::MemoryOAuthClientRepository;
-
-impl OAuthClientRepository for SharedRepository<MemoryOAuthClientRepository> {
-    type Error = ();
-
-    fn save(&mut self, client: OAuthClient) -> Result<(), Self::Error> {
-        self.with_mut(|repository| repository.save(client))
-    }
-
-    fn find_by_id(&self, id: OAuthClientId) -> Result<Option<OAuthClient>, Self::Error> {
-        self.with(|repository| repository.find_by_id(id))
-    }
-
-    fn find_by_client_id(&self, client_id: &str) -> Result<Option<OAuthClient>, Self::Error> {
-        self.with(|repository| repository.find_by_client_id(client_id))
-    }
-
-    fn find_all(&self) -> Result<Vec<OAuthClient>, Self::Error> {
-        self.with(|repository| repository.find_all())
-    }
-}
 
 impl OAuthClientRepository for SharedRepository<PostgresOAuthClientRepository> {
     type Error = DatabaseError;
